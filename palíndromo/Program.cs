dc8ed46fa8293079637aee414f917f6772e1ba54
Zenx007 <zenxpokas@gmail.com>
2025-05-10 23:58:56 -0300

    init

---
+++ palíndromo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Palindromo;
+
+class Verificador
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palavra:");
+        string palavra = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(palavra))
+        {
+            Console.WriteLine("Você não digitou uma palavra válida.");
+            return;
+        }
+
+        string palavraFormatada = palavra.ToLower();
+        string reversa = "";
+
+        for (int i = palavraFormatada.Length - 1; i >= 0; i--)
+        {
+            reversa += palavraFormatada[i];
+        }
+
+        if (palavraFormatada == reversa)
+        {
+            Console.WriteLine("A palavra é um palíndromo!");
+        }
+        else
+        {
+            Console.WriteLine("A palavra NÃO é um palíndromo.");
+        }
+    }
+}
